fed4070529ef606daf89e5598beb7565f85d5e51
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2019-02-28 10:47:32 -0500

    nav links modified

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 var menuButton = document.querySelector('#menu-bt');
 var imgButton = document.querySelector("#img-bt");
-
+var navLinks = document.querySelectorAll('nav a');
 var nav = document.querySelector('nav');
+
+function closeMenu(){
+    menuButton.textContent="";
+    menuButton.classList.remove("closeMenu");
+    menuButton.appendChild(imgButton);
+    nav.style.display ="none";
+}
+
 
 function expendMenu(){
     if (menuButton.textContent !="X"){
@@ -10,12 +18,12 @@ function expendMenu(){
         menuButton.className ="closeMenu";
         nav.style.display ="block";
     }else{
-        menuButton.textContent="";
-        menuButton.classList.remove("closeMenu");
-        menuButton.appendChild(imgButton);
-        nav.style.display ="none";
+        closeMenu();
     }
 }
 
 
 menuButton.addEventListener('click', expendMenu);
+for (var i=0; i<navLinks.length; i+=1){
+    navLinks[i].addEventListener('click', closeMenu);
+}
